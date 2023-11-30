0b469d6d2994c553f2ccb51b9f554cfbae886611
RicardoLucena99 <ricardos.lucena99@gmail.com>
2023-11-30 18:51:09 -0300

    feat: create project and configure eslint and prettier

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text onPress={() => console.log('cara do curso ta mentindoasdasdasd')}>Testando</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
